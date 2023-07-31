aa61c69e72a30093ab1b7ab419fbca91c51d7007
RAJVARDHAN-M <84311650+RAJVARDHAN-M@users.noreply.github.com>
2023-07-31 15:34:58 +0530

    done changes

--- server/index.js
+++ server/index.js
@@ -11,13 +11,13 @@ const app = express();
 
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors(["https://connection-api.onrender.com"]));
+app.use(cors(["https://connection-api.onrender.com","https://connection-9uht.onrender.com"]));
 
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
 const CONNECTION_URL = 'mongodb+srv://rajvardhanmane891:raj@cluster0.pfct39j.mongodb.net/booking?retryWrites=true&w=majority';
-const PORT = process.env.PORT|| 5000;
+const PORT = 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log("Database connected")))
