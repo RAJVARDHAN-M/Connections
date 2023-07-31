3b7e9bec2d23d8b68253dc8fb962ac6dccb16d9b
RAJVARDHAN-M <84311650+RAJVARDHAN-M@users.noreply.github.com>
2023-07-31 16:17:22 +0530

    done changes

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors([]));
+app.use(cors(["https://connection-api.onrender.com","https://connection-9uht.onrender.com"]));
 
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
