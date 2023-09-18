8e18097ff5d2d109c37cfdb2b64fa069295f6eae
RAJVARDHAN-M <84311650+RAJVARDHAN-M@users.noreply.github.com>
2023-09-18 19:00:16 +0530

    Done

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -79,7 +79,7 @@ const SignUp = () => {
             { isSignup ? 'Sign Up' : 'Sign In' }
           </Button>
           <GoogleLogin
-            clientId="564033717568-bu2nr1l9h31bhk9bff4pqbenvvoju3oq.apps.googleusercontent.com"
+            clientId="525287467291-puksm4j9pfdoj7rotou6ncai9tk7uvo4.apps.googleusercontent.com"
             render={(renderProps) => (
               <Button className={classes.googleButton} color="primary" fullWidth onClick={renderProps.onClick} disabled={renderProps.disabled} startIcon={<Icon />} variant="contained">
                 Google Sign In
