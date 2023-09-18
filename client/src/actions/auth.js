507c261138948ba83eefc023da3296024210b124
RAJVARDHAN-M <84311650+RAJVARDHAN-M@users.noreply.github.com>
2023-09-18 16:58:59 +0530

    Done

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,25 +1,28 @@
 import { AUTH } from '../constants/actionTypes';
 import * as api from '../api/index.js';
-
+import {useNavigate} from "react-router-dom"
+  
 export const signin = (formData, router) => async (dispatch) => {
+  const navigate = useNavigate();
   try {
     const { data } = await api.signIn(formData);
 
     dispatch({ type: AUTH, data });
 
-    router.push('/');
+    navigate("/")
   } catch (error) {
     console.log(error);
   }
 };
 
 export const signup = (formData, router) => async (dispatch) => {
+  const navigate = useNavigate();
   try {
     const { data } = await api.signUp(formData);
 
     dispatch({ type: AUTH, data });
 
-    router.push('/');
+    navigate("/")
   } catch (error) {
     console.log(error);
   }
